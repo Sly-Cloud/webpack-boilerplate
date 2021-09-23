4a7e03227d088eea4a52555ce50c0eded7293ea6
Sylvester <nnaemekasilver@gmail.com>
2021-09-23 09:19:47 +0100

    Fix linter

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import _ from 'lodash';
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+
   return element;
 }
 
